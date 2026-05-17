23a8446e02a65bea9f168f03e93fd437de735a5a
Sujith Srikar <kandregulasujith@gmail.com>
2026-05-17 15:41:31 +0530

    feat: implement cached redirect route

---
+++ apps/web/app/[slug]/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import { getRedirectData, setAnalyticsData, setRedirectData } from "@repo/cache";
+import { StatusCode, createErrorResponse } from "@repo/shared";
+import { getLinkBySlug } from '@repo/db'
+
+export async function GET(_: Request, context: { params: Promise<{ slug: string; }> }) {
+  try {
+    const { slug } = await context.params;
+
+    if (!slug) {
+      return NextResponse.json(createErrorResponse("Slug is not given"), { status: StatusCode.BAD_REQUEST, });
+    }
+
+    const cached = await getRedirectData(slug);
+
+    if (cached) {
+      await setAnalyticsData(slug);
+      return NextResponse.redirect(cached.longUrl, 302);
+    }
+
+    const linksData = await getLinkBySlug(slug);
+
+    if (!linksData) {
+      return NextResponse.json(createErrorResponse("Slug not found"), { status: StatusCode.NOT_FOUND, });
+    }
+    const result = await setRedirectData(slug, linksData.longurl);
+
+    if (!result) {
+      return NextResponse.json(createErrorResponse("Unable to cache redirect data"), { status: StatusCode.INTERNAL_SERVER_ERROR, });
+    }
+
+    await setAnalyticsData(slug);
+
+    return NextResponse.redirect(linksData.longurl, 302);
+  } catch (error) {
+    console.log("Error while redirecting:", error);
+
+    return NextResponse.json(createErrorResponse("Error while redirecting"), { status: StatusCode.INTERNAL_SERVER_ERROR, });
+  }
+}
