99b2bdd8ecf7c91d288ad68b2028e46d65505f7b
Sujith Srikar <kandregulasujith@gmail.com>
2026-06-11 07:08:32 +0530

    feat(ui): add InfoTooltip component

---
+++ packages/ui/src/components/infotooltip.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { CircleQuestionMark } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./tooltip";
+
+interface InfoTooltipProps {
+  content: React.ReactNode;
+  className?: string;
+}
+
+export function InfoTooltip({ content, className }: InfoTooltipProps) {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <CircleQuestionMark className={className} size={16} />
+        </TooltipTrigger>
+
+        <TooltipContent>{content}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -6,3 +6,4 @@ export * from "./components/button";
 export * from "./components/input";
 export * from "./components/badge";
 export * from "./components/tooltip";
+export * from './components/infotooltip';
